7eeaf829c914e18bb9ca096b914f62a42b29d3f2
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-09-15 17:09:00 -0400

    Update GridDweller.cs
    Should be good now, fixed A* algorithm

--- Assets/_Dev/CHauser/Scripts/GridDweller.cs
+++ Assets/_Dev/CHauser/Scripts/GridDweller.cs
@@ -69,12 +69,12 @@ public class GridDweller : MonoBehaviour
         // Initilizes the start of the path to be the starting position
         path.Add(positionIndex);
         // Stores the point that is checked and then stored in the path
-        int nextPoint = positionIndex;
+        int currentPoint = positionIndex;
         List<int> potentialPoints = new List<int>();
 
         for (int i = 0; i < grid.mapSize * grid.mapSize * 10; i++)
         {
-            if (nextPoint == positionGoalIndex)
+            if (currentPoint == positionGoalIndex)
             {
                 Debug.Log((DateTime.Now - startTime).TotalMilliseconds); 
                 return;
@@ -84,10 +84,10 @@ public class GridDweller : MonoBehaviour
             
             foreach (Vector3 direction in directions)
             {
-                if (Physics.Raycast(new Vector3(GridSystem.points[nextPoint].x, 1, GridSystem.points[nextPoint].y), direction, out RaycastHit hit, grid.tileSize))
+                if (Physics.Raycast(new Vector3(GridSystem.points[currentPoint].x, 1, GridSystem.points[currentPoint].y), direction, out RaycastHit hit, grid.tileSize))
                     continue;
                     
-                Vector3 hitPoint = new Vector3 (GridSystem.points[nextPoint].x, 0, GridSystem.points[nextPoint].y) + (direction * grid.tileSize);
+                Vector3 hitPoint = new Vector3 (GridSystem.points[currentPoint].x, 0, GridSystem.points[currentPoint].y) + (direction * grid.tileSize);
                 int index = GridSystem.points.IndexOf(new Vector2(hitPoint.x, hitPoint.z));
                 
                 if (index == -1)
@@ -97,57 +97,57 @@ public class GridDweller : MonoBehaviour
             }
 
             bool viablePointFound = false;
-            bool first = true;
+            float gRef = 0;
+            int iterations = 0;
+            foreach (int prev in path)
+            {
+                if(iterations == 0)
+                {
+                    iterations++;
+                    continue;
+                }
+
+                gRef += Mathf.Sqrt(Vectors.SquareDist2f(GridSystem.points[path[iterations - 1]], GridSystem.points[path[iterations]]));
+                iterations++;
+            }
+
+            float bestF = Mathf.Infinity;
+            int bestPoint = -1;
 
             foreach (int point in potentialPoints)
             {
-                if (first)
-                {
-                    if (!path.Contains(point) && !deadEndPoints.Contains(point))
-                    {
-                        first = false;
-                        nextPoint = point;
-                        viablePointFound = true;
-                        continue;
-                    }
-                }
-
-                float dx1 = Mathf.Abs(GridSystem.points[point].x - GridSystem.points[positionGoalIndex].x);
-                float dy1 = Mathf.Abs(GridSystem.points[point].y - GridSystem.points[positionGoalIndex].y);
-                float dx2 = Mathf.Abs(GridSystem.points[nextPoint].x - GridSystem.points[positionGoalIndex].x);
-                float dy2 = Mathf.Abs(GridSystem.points[nextPoint].y - GridSystem.points[positionGoalIndex].y);
+                float dx = Mathf.Abs(GridSystem.points[point].x - GridSystem.points[positionGoalIndex].x);
+                float dy = Mathf.Abs(GridSystem.points[currentPoint].y - GridSystem.points[positionGoalIndex].y);
                 float D = grid.tileSize;
                 float D2 = Mathf.Sqrt(grid.tileSize * grid.tileSize * 2);
-                float h1 = D * (dx1 + dy1) + (D2 - 2 * D) * Mathf.Min(dx1, dy1);
-                float h2 = D * (dx2 + dy2) + (D2 - 2 * D) * Mathf.Min(dx2, dy2);
-                /*
-                F(n) = g(n) + h(n)
-                dx = abs(current_cell.x – goal.x)
-                dy = abs(current_cell.y – goal.y)
- 
-                h = D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
-                where D is length of each node (usually = 1) and D2 is diagonal distance between each node (usually = sqrt(2)). 
-                */
+                float h = D * (dx + dy) + (D2 - 2 * D) * Mathf.Min(dx, dy);
+                float g = gRef + Mathf.Sqrt(Vectors.SquareDist2f(GridSystem.points[point], GridSystem.points[currentPoint]));
                 
-                if (Mathf.Sqrt(Vectors.SqrDist2f(GridSystem.points[point], GridSystem.points[positionGoalIndex])) + h1 < Mathf.Sqrt(Vectors.SqrDist2f(GridSystem.points[nextPoint], GridSystem.points[positionGoalIndex])) + h2 && !path.Contains(point) && !deadEndPoints.Contains(point))
+                
+                if (h + g < bestF && !path.Contains(point) && !deadEndPoints.Contains(point))
                 {
-                    nextPoint = point;
+                    bestF = h + g;
+                    bestPoint = point;
                     viablePointFound = true;
                 }
             }
+            
             if (viablePointFound)
-                path.Add(nextPoint);
+            {
+                currentPoint = bestPoint;
+                path.Add(bestPoint);
+            }
 
             else
             {
                 path.RemoveAt(path.Count - 1);
-                deadEndPoints.Add(nextPoint);
+                deadEndPoints.Add(currentPoint);
                 if (path.Count == 0)
                 {
                     Debug.Log("Goal is unreachable. No path found!");
                     return;
                 }
-                nextPoint = path[path.Count - 1];
+                currentPoint = path[path.Count - 1];
             }
         }
 
