8ddf817d9234d5f509ddba63d569485179f9b4a3
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-11-24 14:17:04 -0500

    Create ActorManager class for actor management
    Added ActorManager class to manage actors in the game.

---
+++ Assets/_Dev/CHauser/Scripts/MonoBehaviour/ActorManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class ActorManager : MonoBehaviour
+{
+  public static List<Actor> allActors = new List<Actor>();
+  public static List<Actor> partyMemberActors = new List<Actor>();
+  public static List<Actor> enemyActors = new List<Actor>();
+
+  private void Start()
+  {
+    allActors = FindObjectsByType<Actor>(FindObjectsSortMode.None).ToList();
+    
+    foreach (Actor actor in allActors)
+    {
+        switch (actor.type)
+        {
+            case Actor.ActorType.Enemy:
+                enemyActors.Add(actor);
+                break;
+    
+            case Actor.ActorType.PartyMember:
+                partyMemberActors.Add(actor);
+                break;
+        }
+    }
+  }
+}*/
