d304567385f551b8d153bc4cc807c64f23becd3d
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-12-02 17:22:46 -0500

    Added A Temp Win / Loss process
    The best place to trigger win or loss is after a death of an actor, whether that be a player character or an enemy, which is why I added this temp logic to this script. This is super simple just to get something that works for EP. I also added a new MonoBehaviour that I intend to move to its' own script tomorrow, I just wrote it here to avoid ".meta" problems. I will also 100% be depreciating the MonoBehaviour after EP in lieu of a more polished level system, death system and etc.

--- Assets/_Dev/CHauser/Scripts/MonoBehaviour/Actor.cs
+++ Assets/_Dev/CHauser/Scripts/MonoBehaviour/Actor.cs
@@ -21,6 +21,7 @@ public class Actor : MonoBehaviour
     public Agent agent;
     [SerializeField] public PlayerInputManager playerInputManager;
 
+    // TempWinLossManagerEP winLossManager;
 
     private void Start()
     {
@@ -33,6 +34,7 @@ public class Actor : MonoBehaviour
         }
 
         playerInputManager = FindFirstObjectByType<PlayerInputManager>();
+        // winLossManager = FindFirstObjectByType<TempWinLossManagerEP>();
     }
 
     public void TakeDamage(float damage)
@@ -47,6 +49,43 @@ public class Actor : MonoBehaviour
         ActorManager.allActors.Remove(this);
         ActorManager.partyMemberActors.Remove(this);
         ActorManager.enemyActors.Remove(this);
+        
+        if (ActorManager.enemyActors.Count == 0)
+            // winLossManager.Win();
+            
+        else if(ActorManager.partyMemberActors.Count == 0)
+            // winLossManager.Loss();
+            
         Destroy(this.gameObject);
     }
 }
+/*
+// Temporary win / loss screen manager, will move to its' own script, I just don't want .meta file conflicts to occur again.
+// Will definitly depricate in the future in favor of a more complex level fail / succeed script, but this will work for now for Experience Pinellas.
+// Also def gonna comment this out so that the team doesn't have to do the compiler error shuffle tomorrow.
+// Also gonna include these comments in the new script that I'm going to migrate to tommorow. 
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempWinLossManagerEP : MonoBehaviour // EP stands for Experience Pinellas.
+{
+    [SerializeField] GameObject winScreen;
+    [SerializeField] GameObject lossScreen;
+    
+    public void Win()
+    {
+        winScreen.SetActive(true);
+    }
+
+    public void Loss()
+    {
+        lossScreen.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}*/
