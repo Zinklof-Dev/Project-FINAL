28e10aabde85f2ef6201f70ce276bebdb7db35e8
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-12-02 18:46:20 -0500

    Restructured item class
    Going to update Item class to Tool or maybe Weapon with new properties. Part of basic card / item system for EP.

--- Assets/_Dev/CHauser/Scripts/Classes/Item.cs
+++ Assets/_Dev/CHauser/Scripts/Classes/Item.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
 
-public class Item
+// Going to rename class and file, goingto do so tomorrow in Flower's though bc of compiler and .meta errors.
+// Going to delete "Weapon" class or maybe scrap "Tool" and move it to there, still need to decide though.
+
+public class Item // Tool // Tool is new name for this class and file
 {
-    public enum Type { Consumable, Weapon }
-    public Type type;
-    public int id;
-    public string name;
-    public string description;
+    // Needed maybe
+    public enum Type { Axe, Sword, Default }
+    private Type type;
+    // Following going to stay unused probably for EP
+    private int id;
+    private string name;
+    private string description;
+    // These will be needed essential variables
+    private float attackPower; 
+    private float reach
+
+    public Item(Type type, float attackPower, float reach)
+    {
+        this.type = type;
+        this.attackPower = attackPower;
+        this.reach = reach;
+        // TEMP
+        id = 69420;
+        name = "Pizza Maker";
+        description = "KIKI ZIZI KINKY ZINKY";
+    }
 }
