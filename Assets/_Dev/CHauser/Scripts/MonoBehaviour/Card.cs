6c26ed2aa0f9041a4c755358d3ad3adbb523a0a1
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-12-02 19:29:42 -0500

    Code for card doing stuff

--- Assets/_Dev/CHauser/Scripts/MonoBehaviour/Card.cs
+++ Assets/_Dev/CHauser/Scripts/MonoBehaviour/Card.cs
@@ -4,8 +4,22 @@ using ZinklofDev.Utils.MathZ;
 
 public class Card : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    public enum ItemType { Null, ReachPotion, HealthPotion, ActionPointBoostPotion, Axe, Sword };
+    [SerializeField] public ItemType itemType;
+
+    // Used if a tool / weapon, reach also used if reach potion
+    [SerializeField] private float attackPower;
+    [SerializeField] private float reach;
+
+    // Used if potions
+    [SerializeField] private float health;
+    [SerializeField] private float actionPoints;
+
+    [SerializeField] private GameObject actionPointBoostPotionPrompt;
+
     RectTransform rectTransform;
     TurnManager turnManager;
+    PlayerInputManager playerInputManager;
     Actor currentSelectedActor;
     bool actorFound = false;
 
@@ -13,19 +27,59 @@ public class Card : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndD
     {
         rectTransform = GetComponent<RectTransform>();
         turnManager = FindFirstObjectByType<TurnManager>();
+        playerInputManager = FindFirstObjectByType<PlayerInputManager>();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
         
     }
+    
     public void OnBeginDrag(PointerEventData eventData)
     {
 
     }
+    
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        switch (itemType)
+        {
+            case ItemType.ReachPotion:
+            ReachPotionAction();
+            break;
+            
+            case ItemType.HealthPotion:
+            HealthPotionAction();
+            break;
+            
+            case ItemType.ActionPointBoostPotion:
+            ActionPointPotionAction();
+            break;
+            
+            case ItemType.Axe:
+            AxeAction();
+            break;
+            
+            case ItemType.Sword:
+            SwordAction();
+            break;
+        }
+        
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(playerInputManager.state  != PlayerInputManager.State.SelectingPartyMember)
             return;
+
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void ReachPotionAction()
+    {
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        {
+            CardManager.SnapCardBack(this);
+            return;
+        }
 
         float x = Numbers.RoundToMultiple(hit.point.x, GridSystem.staticTileSize, GridSystem.staticOffsetX);
         float y = Numbers.RoundToMultiple(hit.point.z, GridSystem.staticTileSize, GridSystem.staticOffsetY);
@@ -43,19 +97,141 @@ public class Card : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndD
                 break;
             }
         }
+        
         if (!actorFound) 
             CardManager.SnapCardBack(this);
-
         else
         {
-            // Attatch item to Actor
+            currentSelectedActor.range += reach;
+            Destroy(this.gameObject);
         }
     }
-    public void OnDrag(PointerEventData eventData)
-    {
-        if(turnManager.currentTurn == TurnManager.Turn.EnemyTurn)
-            return;
 
-        rectTransform.anchoredPosition += eventData.delta;
+    public void HealthPotionAction()
+    {
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        {
+            CardManager.SnapCardBack(this);
+            return;
+        }
+
+        float x = Numbers.RoundToMultiple(hit.point.x, GridSystem.staticTileSize, GridSystem.staticOffsetX);
+        float y = Numbers.RoundToMultiple(hit.point.z, GridSystem.staticTileSize, GridSystem.staticOffsetY);
+
+        Vector2 selectedPosition = new Vector2(x, y);
+
+        actorFound = false;
+
+        foreach (Actor actor in ActorManager.partyMemberActors)
+        {
+            if (actor.agent.currentIndex == GridSystem.points.IndexOf(selectedPosition))
+            {
+                currentSelectedActor = actor;
+                actorFound = true;
+                break;
+            }
+        }
+        
+        if (!actorFound) 
+            CardManager.SnapCardBack(this);
+            
+        else
+        {
+            currentSelectedActor.health += health;
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ActionPointPotionAction()
+    {
+        actionPointBoostPotionPrompt.SetActive(true);
+    }
+
+    public void ActionPointPotion(bool use)
+    {
+        if (use)
+        {
+            turnManager.TakeAction(-actionPoints);
+            return;
+        }
+        
+        CardManager.SnapCardBack(this);
+    }
+
+    public void AxeAction()
+    {
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        {
+            CardManager.SnapCardBack(this);
+            return;
+        }
+
+        float x = Numbers.RoundToMultiple(hit.point.x, GridSystem.staticTileSize, GridSystem.staticOffsetX);
+        float y = Numbers.RoundToMultiple(hit.point.z, GridSystem.staticTileSize, GridSystem.staticOffsetY);
+
+        Vector2 selectedPosition = new Vector2(x, y);
+
+        actorFound = false;
+
+        foreach (Actor actor in ActorManager.partyMemberActors)
+        {
+            if (actor.agent.currentIndex == GridSystem.points.IndexOf(selectedPosition))
+            {
+                currentSelectedActor = actor;
+                actorFound = true;
+                break;
+            }
+        }
+        
+        if (!actorFound) 
+            CardManager.SnapCardBack(this);
+            
+        else if (currentSelectedActor.equippedTool != null)
+            CardManager.SnapCardBack(this);
+            
+        else
+        {
+            currentSelectedActor.equippedTool = new Item(Item.Type.Axe, attackPower, reach);
+            Destroy(this.gameObject);
+        }
+    }
+    }
+
+    public void SwordAction()
+    {
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        {
+            CardManager.SnapCardBack(this);
+            return;
+        }
+
+        float x = Numbers.RoundToMultiple(hit.point.x, GridSystem.staticTileSize, GridSystem.staticOffsetX);
+        float y = Numbers.RoundToMultiple(hit.point.z, GridSystem.staticTileSize, GridSystem.staticOffsetY);
+
+        Vector2 selectedPosition = new Vector2(x, y);
+
+        actorFound = false;
+
+        foreach (Actor actor in ActorManager.partyMemberActors)
+        {
+            if (actor.agent.currentIndex == GridSystem.points.IndexOf(selectedPosition))
+            {
+                currentSelectedActor = actor;
+                actorFound = true;
+                break;
+            }
+        }
+        
+        if (!actorFound) 
+            CardManager.SnapCardBack(this);
+            
+        else if (currentSelectedActor.equippedTool != null)
+            CardManager.SnapCardBack(this);
+            
+        else
+        {
+            currentSelectedActor.equippedTool = new Item(Item.Type.Sword, attackPower, reach);
+            Destroy(this.gameObject);
+        }
     }
 }
