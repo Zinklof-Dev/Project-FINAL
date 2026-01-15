78e83df170f169f85d393a308c8f843bc0c173e0
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2026-01-15 08:54:02 -0500

    Starting logic for the party to persist through scenes.

--- Assets/_Dev/CHauser/Scripts/Classes/PlayableCharacter.cs
+++ Assets/_Dev/CHauser/Scripts/Classes/PlayableCharacter.cs
@@ -3,7 +3,9 @@ using static Actor;
 
 public class PlayableCharacter
 {
-    ActorType type;
+    public static List<PlayableCharacter> partyMembers = new List<PlayableCharacter>();
+    
+    ActorType type = ActorType.PartyMember;
     public string name;
     public string background;
     public float health;
@@ -13,5 +15,34 @@ public class PlayableCharacter
     public float attackPower;
     public List<Item> actorInventory;
     public List<Item> equippedItems;
-    public int ID { get;  private set; }
+    public Item equippedTool = null;
+
+    public PlayableCharacter(string name, string background, float maxHealth, float speed, float range, float attackPower)
+    {
+        this.name = name;
+        this.background = background;
+        this.maxHealth = maxHealth;
+        health = maxHealth;
+        this.speed = speed;
+        this.range = range;
+        this.attackPower = attackPower;
+        actorInventory = new List<Item>();
+        equippedItems = new List<Item>();
+        
+        partyMembers.Add(this);
+    }
+
+    public void UpdateData(string name, string background, float maxHealth, float health, float speed, float range, float attackPower, List<Item> actorInventory, List<Item> equippedItems, Item equippedTool)
+    {
+        this.name = name;
+        this.background = background;
+        this.maxHealth = maxHealth;
+        this.health = health;
+        this.speed = speed;
+        this.range = range;
+        this.attackPower = attackPower;
+        this.actorInventory = actorInventory;
+        this.equippedItems = equippedItems;
+        this.equippedTool = equippedTool;
+    }
 }
