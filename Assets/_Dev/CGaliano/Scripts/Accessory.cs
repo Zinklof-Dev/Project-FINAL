fca8cccaf87388f373034fc19d3c7a3a999e909c
Zink <zinklofbusiness@gmail.com>
2026-02-19 11:18:31 -0500

    Create Accessory.cs

---
+++ Assets/_Dev/CGaliano/Scripts/Accessory.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace BOTD.CharacterModels
+{
+    [Serializeable]
+    public struct Accessory
+    {
+        public Mesh mesh;
+        public string name;
+    }
+}
