77f842bec244d6d7db52e01a4d038ec3baa2a77a
Zinklof <123103261+Zinklof@users.noreply.github.com>
2026-04-01 09:44:39 -0400

    textures and UVs galore

--- Assets/!Bastion/Engine/LaunchHandler/BastionMonoManager.cs
+++ Assets/!Bastion/Engine/LaunchHandler/BastionMonoManager.cs
@@ -32,7 +32,7 @@ namespace Bastion.LaunchHandler
         /// </summary>
         public void Initialize()
         {
-            Debug.Log("FUCK FUCK FUCK FUCK!");
+            //Debug.Log("FUCK FUCK FUCK FUCK!");
 
             // if the bastion canvas exists already destroy it so we don't cause problems in the event we need to reinitialize
             if (Bastion.Engine.canvas != null)
